fd7ab0224e170b10191d8d596a1d477cd108236e
fardiab <feridabd37@gmail.com>
2023-06-21 11:53:24 +0400

    filtering countries

--- app/apis/views.py
+++ app/apis/views.py
@@ -3,7 +3,13 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from core.models import Sect, SubSect, Indica, Country
-from .serializers import SectSerializer, SubSectSerializer, IndicaSerializer, CountrySerializer
+from .serializers import (
+    SectSerializer,
+    SubSectSerializer,
+    IndicaSerializer,
+    CountrySerializer,
+)
+
 
 class SectViewSet(viewsets.ModelViewSet):
     serializer_class = SectSerializer
@@ -11,8 +17,7 @@ class SectViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         queryset = Sect.objects.all()
         return queryset
-    
-    
+
 
 class SubSectApiView(APIView):
     serializer_class = SubSectSerializer
@@ -21,30 +26,35 @@ class SubSectApiView(APIView):
         subsector = SubSect.objects.all()
         serializer = SubSectSerializer(subsector, many=True)
         return Response(serializer.data)
-    
+
+
 class IndicaApiView(APIView):
     serializer_class = IndicaSerializer
 
     def get(self, request, pk=None):
         if pk is not None:
             try:
-                sector = Sect.objects.prefetch_related('subsect_set__indica_set').get(pk=pk)
+                sector = Sect.objects.prefetch_related("subsect_set__indica_set").get(
+                    pk=pk
+                )
                 data = []
                 for subsector in sector.subsect_set.all():
                     indicators = subsector.indica_set.all()
                     indicator_data = [
-                        {'id': indicator.id, 'indicator': indicator.indicator}
+                        {"id": indicator.id, "indicator": indicator.indicator}
                         for indicator in indicators
                     ]
-                    data.append({
-                        'id': subsector.id,
-                        'subsector': subsector.subsector,
-                        'indicators': indicator_data
-                    })
+                    data.append(
+                        {
+                            "id": subsector.id,
+                            "subsector": subsector.subsector,
+                            "indicators": indicator_data,
+                        }
+                    )
                 sector_data = {
-                    'id': sector.id,
-                    'sector': sector.sector,
-                    'subsectors': data
+                    "id": sector.id,
+                    "sector": sector.sector,
+                    "subsectors": data,
                 }
                 return Response(sector_data)
             except Sect.DoesNotExist:
@@ -57,18 +67,20 @@ class IndicaApiView(APIView):
                 for subsector in sector.subsect_set.all():
                     indicators = subsector.indica_set.all()
                     indicator_data = [
-                        {'id': indicator.id, 'indicator': indicator.indicator}
+                        {"id": indicator.id, "indicator": indicator.indicator}
                         for indicator in indicators
                     ]
-                    subsector_data.append({
-                        'id': subsector.id,
-                        'subsector': subsector.subsector,
-                        'indicators': indicator_data
-                    })
+                    subsector_data.append(
+                        {
+                            "id": subsector.id,
+                            "subsector": subsector.subsector,
+                            "indicators": indicator_data,
+                        }
+                    )
                 sector_data = {
-                    'id': sector.id,
-                    'sector': sector.sector,
-                    'subsectors': subsector_data
+                    "id": sector.id,
+                    "sector": sector.sector,
+                    "subsectors": subsector_data,
                 }
                 data.append(sector_data)
             return Response(data)
@@ -77,10 +89,21 @@ class IndicaApiView(APIView):
 class CountryApiView(APIView):
     serializer_class = CountrySerializer
 
-    def get(self, request, pk=None):
+    def get(self, request):
+        count = int(request.GET.get("count", 0))
+        selected_country = request.GET.getlist("country")
+        year = request.GET.get("year", None)
+
         queryset = Country.objects.all()
+
+        if selected_country:
+            queryset = Country.objects.filter(country__in=selected_country)
+
+        if year:
+            queryset = queryset.filter(year=year)
+
+        if count:
+            queryset = queryset.order_by("-id")[:count]
+
         serializer = CountrySerializer(queryset, many=True)
         return Response(serializer.data)
-
-
-
