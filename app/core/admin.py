239b1b2136c90e3e21ba220a2d1caf9930a58c59
fardiab <feridabd37@gmail.com>
2023-06-21 11:54:50 +0400

    adding country model

--- app/core/admin.py
+++ app/core/admin.py
@@ -5,4 +5,8 @@ from .models import Sect, SubSect, Indica, Country
 admin.site.register(Sect)
 admin.site.register(SubSect)
 admin.site.register(Indica)
-admin.site.register(Country)
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('country', 'rank', 'amount', 'year')
+    search_fields = ('country', 'year')
