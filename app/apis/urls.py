b7ed06e02845d9f015a57ed2f548893ca1d638ee
fardiab <feridabd37@gmail.com>
2023-06-26 11:31:38 +0400

    adding ran diff

--- app/apis/urls.py
+++ app/apis/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('indicators/',  views.IndicaApiView.as_view(), name='indicators'),
     path('indicators/<int:pk>/', views.IndicaApiView.as_view(), name='indicators-detail'),
     path('country/', views.CountryApiView.as_view(), name='country'),
+    path('rank-difference/', views.RankDifferenceApiView.as_view(), name='rank_difference'),
     # path('country-ranks/<int:pk>/', views.CountryRankApiView.as_view(), name='country-ranks-detail'),
 ]
