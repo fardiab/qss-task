e75b3ea528adb768e2fdf51504d74e02aeae2e3e
fardiab <feridabd37@gmail.com>
2023-07-20 14:51:51 +0400

    fixing CountryScoreYear api

--- app/apis/views/CountryScoreYearByApiView.py
+++ app/apis/views/CountryScoreYearByApiView.py
@@ -11,33 +11,56 @@ class CountryScoreYearByApiView(APIView):
 
     def get(self, request):
         country = request.GET.get("country")
+        sector = request.GET.get("sector")
+        year = request.GET.get("year")
 
         indicators_data = (
-            Country.objects.filter(country=country)
+            Country.objects.filter(
+                country=country, indicator__subsector__sector__sector=sector
+            )
             .prefetch_related("indicator__subsector__sector")
-            .values("year", "rank", "indicator__subsector__sector__sector")
+            .values(
+                "year",
+                "rank",
+                "indicator__subsector__sector__sector",
+                "indicator__indicator",
+            )
         )
+
+        if sector:
+            indicators_data = [
+                data
+                for data in indicators_data
+                if data["indicator__subsector__sector__sector"] == sector
+            ]
 
         year_rank_dict = defaultdict(list)
         for data in indicators_data:
-            year = data["year"]
             rank = data["rank"]
-            sector_name = data["indicator__subsector__sector__sector"]
+            year = data["year"]
             year_rank_dict[year].append(rank)
+
+        max_rank_dict = {}
+        for year, ranks in year_rank_dict.items():
+            max_rank_year = max(ranks)
+            max_rank_dict[year] = max_rank_year
 
         response_data = []
         for year, ranks in year_rank_dict.items():
-            max_rank_year = max(ranks)
-            rank_sum = sum(ranks)
-            average_rank = rank_sum / len(ranks)
-            average_rank = round(average_rank, 2)
-            score = round((1 - average_rank / max_rank_year) * 100, 2)
-            year_info = {
-                "year": year,
-                "score": score,
-            }
-            response_data.append(year_info)
+            max_rank_year = max_rank_dict[year]
+            if ranks:
+                rank_sum = sum(ranks)
+                average_rank = rank_sum / len(ranks)
+                average_rank = round(average_rank, 2)
+                score = round((1 - average_rank / max_rank_year) * 100, 2)
 
-        response_data.sort(key=itemgetter("score"), reverse=True) 
+                response_data.append(
+                    {
+                        "year": year,
+                        "average_score": score,
+                    }
+                )
+
+        response_data = sorted(response_data, key=itemgetter("year"))
 
         return Response(response_data)
